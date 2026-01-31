4432ced7a1e64d116682b0c6d204c4ce14f8b247
Coutons <coutons@example.com>
2026-02-01 04:38:14 +0700

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,33 +1,37 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-import preact from '@astrojs/preact';
-import sitemap from '@astrojs/sitemap';
-import remarkToc from 'remark-toc';
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import preact from "@astrojs/preact";
+import sitemap from "@astrojs/sitemap";
+import remarkToc from "remark-toc";
 
 export default defineConfig({
-  site: 'https://courseswyn.com',
-  output: 'static',
+  site: "https://courseswyn.com",
+  base: "/",
+  output: "static",
   integrations: [
     tailwind(),
     preact(),
     sitemap({
-      changefreq: 'weekly',
+      changefreq: "weekly",
       priority: 0.8,
       entryLimit: 5000,
-      lastmod: new Date()
-    })
+      lastmod: new Date(),
+    }),
   ],
   markdown: {
     remarkPlugins: [
-      [remarkToc, {
-        heading: 'Table of Contents',
-        maxDepth: 3,
-        tight: true
-      }]
-    ]
+      [
+        remarkToc,
+        {
+          heading: "Table of Contents",
+          maxDepth: 3,
+          tight: true,
+        },
+      ],
+    ],
   },
   experimental: {
-    svgo: true
-  }
+    svgo: true,
+  },
 });
